4b9fd4ec7818e56738dd782d21da3474ec792665
Karim <shakirovcorporation@gmail.com>
2022-06-16 16:25:48 +0600

    updateProject

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-function dontGiveMeFive(start, end) {
-    let arr = [];
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
-    for (let i = start; i <= end; i++) {
-        if (i.toString().includes('5') === false) arr.push(i)
-    }
-    return arr.length;
-}
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
 
 
-console.log(dontGiveMeFive(1,40))
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
