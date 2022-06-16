afd40ed0fe28facd344b9224016c014284eab787
Karim <shakirovcorporation@gmail.com>
2022-06-16 14:28:11 +0600

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('arr' + ' - object');
+console.log('arr' + 'Karim');
+console.log(4 + +'5')
+
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr)
+console.log(decr)
+
+
+
+
+
+
+
+
+
+
+
