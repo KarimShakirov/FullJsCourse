b194bd9b35265ed4ebb375b44fd1f47090ed75b3
Karim <shakirovcorporation@gmail.com>
2022-06-17 13:23:06 +0600

    function

---
+++ func.js
@@ -0,0 +1,6 @@
+function getSumNumber(num1, num2) {
+    return num1 + num2;
+}
+
+
+console.log(getSumNumber(5, 2));
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '')
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -6,27 +6,50 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+}
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", '');
+//     const b = prompt("На сколько оцените его?", '');
+//
+//     if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+let i = 0;
+
+while (i < 2) {
+    const a = prompt("Один из последних просмотренных фильмов?", '');
+    const b = prompt("На сколько оцените его?", '');
+
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        console.log('DONE')
+        personalMovieDB.movie[a] = b;
+    } else {
+        console.log("ERROR")
+        i--;
+    }
+}
+
+
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель")
+} else if (personalMovieDB.count >= 30){
+    console.log("ВЫ КИНОМАН!")
+} else {
+    console.log("Произошла ошибка")
+}
 
 console.log(personalMovieDB)
-
-
-
-
-
-
-
-
-
 
 
 
